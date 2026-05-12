1a6c0c78d4d0f23204e7d1819f520c4c09f013c3
Victor Antonio Guaipo Mendoza <vguaipo@rolda.com.ve>
2026-05-12 16:29:45 -0400

    Last Minute change

--- src/data/reglamento.ts
+++ src/data/reglamento.ts
@@ -1,6 +1,17 @@
 export const reglamento = [
-    {id: 1, 
-     name: "",
-     url: "/pdf/reglamento/1. Principios y Relaciones de trabajo.pdf"
-    }
+    {
+        id: 1,
+        name: "1. Principios y Relaciones de Trabajo",
+        url: "/pdf/reglamento/1. Principios y Relaciones de trabajo.pdf"
+    },
+    {
+        id: 2,
+        name: "2. Cosméticos Rolda, C.A",
+        url: "/pdf/reglamento/2. Cosméticos Rolda, C.A"
+    },
+    {
+        id: 3,
+        name: "3. Misión y Visión",
+        url: "/pdf/reglamento/3. Misión y Visión"
+    },
 ]
